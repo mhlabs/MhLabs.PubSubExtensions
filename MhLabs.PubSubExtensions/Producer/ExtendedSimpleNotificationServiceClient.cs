1ca3f85a3c5389652b0785d2d1b83444b1bacfb8
Lars <lars@mathem.se>
2018-10-09 15:18:57 +0200

    propagating ctors

--- MhLabs.PubSubExtensions/Producer/ExtendedSimpleNotificationServiceClient.cs
+++ MhLabs.PubSubExtensions/Producer/ExtendedSimpleNotificationServiceClient.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon;
+using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.SimpleNotificationService;
@@ -17,11 +18,22 @@ namespace MhLabs.PubSubExtensions.Producer
     public class ExtendedSimpleNotificationServiceClient : AmazonSimpleNotificationServiceClient
     {
         public MessageDeliverySettings _messageDeliverySettings = new MessageDeliverySettings();
-        public IAmazonS3 _s3Client = new AmazonS3Client(RegionEndpoint.EUWest1);//.GetBySystemName(Environment.GetEnvironmentVariable("DEFAULT_AWS_REGION")));
-        public IAmazonStepFunctions _stepFunctions = new AmazonStepFunctionsClient(RegionEndpoint.EUWest1);//.GetBySystemName(Environment.GetEnvironmentVariable("DEFAULT_AWS_REGION")));
+        public IAmazonS3 _s3Client = new AmazonS3Client();
+        public IAmazonStepFunctions _stepFunctions = new AmazonStepFunctionsClient();
 
-        public ExtendedSimpleNotificationServiceClient(RegionEndpoint region) : base(region)
-        { }
+        public ExtendedSimpleNotificationServiceClient() : base() { }
+        public ExtendedSimpleNotificationServiceClient(RegionEndpoint region) : base(region) { }
+        public ExtendedSimpleNotificationServiceClient(AmazonSimpleNotificationServiceConfig config) : base(config) { }
+        public ExtendedSimpleNotificationServiceClient(AWSCredentials credentials) : base(credentials) { }
+        public ExtendedSimpleNotificationServiceClient(AWSCredentials credentials, RegionEndpoint region) : base(credentials, region) { }
+        public ExtendedSimpleNotificationServiceClient(AWSCredentials credentials, AmazonSimpleNotificationServiceConfig clientConfig) : base(credentials, clientConfig) { }
+        public ExtendedSimpleNotificationServiceClient(string awsAccessKeyId, string awsSecretAccessKey) : base(awsAccessKeyId, awsSecretAccessKey) { }
+        public ExtendedSimpleNotificationServiceClient(string awsAccessKeyId, string awsSecretAccessKey, RegionEndpoint region) : base(awsAccessKeyId, awsSecretAccessKey, region) { }
+        public ExtendedSimpleNotificationServiceClient(string awsAccessKeyId, string awsSecretAccessKey, string awsSessionToken) : base(awsAccessKeyId, awsSecretAccessKey, awsSessionToken) { }
+        public ExtendedSimpleNotificationServiceClient(string awsAccessKeyId, string awsSecretAccessKey, AmazonSimpleNotificationServiceConfig clientConfig) : base(awsAccessKeyId, awsSecretAccessKey, clientConfig) { }
+        public ExtendedSimpleNotificationServiceClient(string awsAccessKeyId, string awsSecretAccessKey, string awsSessionToken, AmazonSimpleNotificationServiceConfig clientConfig) : base(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, clientConfig) { }
+        public ExtendedSimpleNotificationServiceClient(string awsAccessKeyId, string awsSecretAccessKey, string awsSessionToken, RegionEndpoint region) : base(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, region) { }
+
 
         public override async Task<PublishResponse> PublishAsync(PublishRequest request, CancellationToken cancellationToken = default(CancellationToken))
         {
