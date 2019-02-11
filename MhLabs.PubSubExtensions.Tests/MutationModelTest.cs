1ac2a3a94a7e70a42424aef4113c6dc992354fc9
Mattias Liljenzin <mattias.liljenzin@gmail.com>
2019-02-11 16:19:00 +0100

    feat: add failing tests to verify 'System.Reflection.TargetParameterCountException : Parameter count mismatch.' when using nested List<T>

--- MhLabs.PubSubExtensions.Tests/MutationModelTest.cs
+++ MhLabs.PubSubExtensions.Tests/MutationModelTest.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using AutoFixture;
 using MhLabs.PubSubExtensions.Model;
 using Xunit;
 
@@ -81,6 +83,66 @@ namespace MhLabs.PubSubExtensions.Tests
             Assert.True(diff.Any(p => p == "Address"));
             Assert.True(diff.Any(p => p == "Address.AddressRow1"));
         }
+
+        [Fact]
+        public void NestedListNotChanged()
+        {
+            var obj1 = new TestItem { Name = "Test", Age = 10, CreationDate = DateTime.Today, Address = new TestAddress { AddressRow1 = "B" } };
+            var obj2 = new TestItem { Name = "Test", Age = 10, CreationDate = DateTime.Today, Address = new TestAddress { AddressRow1 = "C" } };
+
+            var fixture = new Fixture();
+            var minis = fixture.CreateMany<TestMiniItem>(10).ToList();
+
+            obj1.Minis = minis;
+            obj2.Minis = minis;
+
+            var model = new MutationModel<TestItem> { OldImage = obj1, NewImage = obj2 };
+            var diff = model.Diff();
+            Assert.Equal(2, diff.Count);
+            Assert.True(diff.Any(p => p == "Address"));
+            Assert.True(diff.Any(p => p == "Address.AddressRow1"));
+        }
+
+        [Fact]
+        public void NestedListQuantityChanged()
+        {
+            var obj1 = new TestItem { Name = "Test", Age = 10, CreationDate = DateTime.Today, Address = new TestAddress { AddressRow1 = "B" } };
+            var obj2 = new TestItem { Name = "Test", Age = 10, CreationDate = DateTime.Today, Address = new TestAddress { AddressRow1 = "C" } };
+
+            var fixture = new Fixture();
+            var minis = fixture.CreateMany<TestMiniItem>(10).ToList();
+
+            obj1.Minis = minis;
+            obj2.Minis = minis.Skip(1).ToList(); 
+
+            var model = new MutationModel<TestItem> { OldImage = obj1, NewImage = obj2 };
+            var diff = model.Diff();
+            Assert.Equal(3, diff.Count);
+            Assert.True(diff.Any(p => p == "Address"));
+            Assert.True(diff.Any(p => p == "Address.AddressRow1"));
+            Assert.True(diff.Any(p => p == "Minis"));
+        }
+
+        [Fact]
+        public void NestedListContentChanged()
+        {
+            var obj1 = new TestItem { Name = "Test", Age = 10, CreationDate = DateTime.Today, Address = new TestAddress { AddressRow1 = "B" } };
+            var obj2 = new TestItem { Name = "Test", Age = 10, CreationDate = DateTime.Today, Address = new TestAddress { AddressRow1 = "C" } };
+
+            var fixture = new Fixture();
+            var minis = fixture.CreateMany<TestMiniItem>(10).ToList();
+
+            obj1.Minis = minis;
+            obj2.Minis = minis.Skip(1).ToList(); 
+            obj2.Minis.Add(fixture.Create<TestMiniItem>());
+
+            var model = new MutationModel<TestItem> { OldImage = obj1, NewImage = obj2 };
+            var diff = model.Diff();
+            Assert.Equal(3, diff.Count);
+            Assert.True(diff.Any(p => p == "Address"));
+            Assert.True(diff.Any(p => p == "Address.AddressRow1"));
+            Assert.True(diff.Any(p => p == "Minis"));
+        }
     }
 
     internal class TestItem
@@ -89,8 +151,14 @@ namespace MhLabs.PubSubExtensions.Tests
         public int Age { get; set; }
         public DateTime CreationDate { get; set; }
         public TestAddress Address { get; set; }
+        public List<TestMiniItem> Minis { get; set; }
     }
 
+    internal class TestMiniItem
+    {
+        public string Id { get; set; }
+        public string Message { get; set; }
+    }
 
     public class TestAddress
     {
