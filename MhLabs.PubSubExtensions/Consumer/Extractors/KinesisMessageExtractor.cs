752554fa89d5683ff805288da0a196758d33ec63
Viktor Stenqvist <viktor.stenqvist@mathem.se>
2019-10-21 11:48:05 +0200

    Whitespace fix

--- MhLabs.PubSubExtensions/Consumer/Extractors/KinesisMessageExtractor.cs
+++ MhLabs.PubSubExtensions/Consumer/Extractors/KinesisMessageExtractor.cs
@@ -12,7 +12,7 @@ namespace MhLabs.PubSubExtensions.Consumer.Extractors
         public Type ExtractorForType => typeof(KinesisEvent);
 
         public async Task<IEnumerable<TMessageType>> ExtractEventBody<TEventType, TMessageType>(TEventType ev)  where TMessageType : class, new() {
-            var kinesisEvent = ev as KinesisEvent;            
+            var kinesisEvent = ev as KinesisEvent;
             return await Task.FromResult(kinesisEvent.Records.Select(p => p.Kinesis.Data.DeserializeStream<TMessageType>()));
         }
 
