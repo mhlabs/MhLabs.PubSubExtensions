2123500efd10fe1eba5eb0e21db327c61a930ea3
calledude <22471295+calledude@users.noreply.github.com>
2020-01-16 19:37:16 +0100

    Implement SQSProcessor

---
+++ MhLabs.PubSubExtensions/Consumer/SQSProcessor.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.Lambda.SQSEvents;
+using Amazon.S3;
+using MhLabs.PubSubExtensions.Consumer.Extractors;
+using Microsoft.Extensions.Logging;
+
+namespace MhLabs.PubSubExtensions.Consumer
+{
+    public abstract class SQSProcessor<TMessage> : MessageProcessorBase<SQSEvent, TMessage>
+        where TMessage : class, new()
+    {
+        protected SQSProcessor(IAmazonS3 s3Client = null, ILoggerFactory loggerFactory = null) : base(s3Client, loggerFactory)
+        {
+            RegisterExtractor(new SQSMessageExtractor<TMessage>());
+        }
+    }
+}
