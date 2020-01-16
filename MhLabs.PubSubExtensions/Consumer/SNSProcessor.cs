1136bcabe8cbf44766f9f3878e66f61686f9bf48
calledude <22471295+calledude@users.noreply.github.com>
2020-01-16 19:37:06 +0100

    Implement SNSProcessor

---
+++ MhLabs.PubSubExtensions/Consumer/SNSProcessor.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.Lambda.SNSEvents;
+using Amazon.S3;
+using MhLabs.PubSubExtensions.Consumer.Extractors;
+using Microsoft.Extensions.Logging;
+
+namespace MhLabs.PubSubExtensions.Consumer
+{
+    public abstract class SNSProcessor<TMessage> : MessageProcessorBase<SNSEvent, TMessage>
+        where TMessage : class, new()
+    {
+        protected SNSProcessor(IAmazonS3 s3Client = null, ILoggerFactory loggerFactory = null) : base(s3Client, loggerFactory)
+        {
+            RegisterExtractor(new SNSMessageExtractor<TMessage>());
+        }
+    }
+}
